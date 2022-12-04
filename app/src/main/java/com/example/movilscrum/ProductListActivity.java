2f47e6a5281c35fffc88a13963528512d41887ed
patman89 <75132741+patman89@users.noreply.github.com>
2022-12-04 13:01:18 -0500

    Elaboration description products-04-12-2022

--- app/src/main/java/com/example/movilscrum/ProductListActivity.java
+++ app/src/main/java/com/example/movilscrum/ProductListActivity.java
@@ -22,15 +22,15 @@ public class ProductListActivity extends AppCompatActivity {
         listViewProducts = (ListView) findViewById(R.id.listViewProducts);
         products = new ArrayList<>();
         AddProduct(R.drawable.regla,"Regla","Regla color amarilla 50 cm","1000");
-        AddProduct(R.drawable.tijeras,"Tijeras","Iijeras color azul","1000");
+        AddProduct(R.drawable.tijeras,"Tijeras","Iijeras color azul","2000");
         AddProduct(R.drawable.lapiz,"Lapiz","Lapiz amarillo No2","1500");
-        AddProduct(R.drawable.cuaderno,"Cuaderno","Cuaderno descripcion","1200");
-        AddProduct(R.drawable.carpeta,"Carpeta","Carpeta descripcion","1800");
-        AddProduct(R.drawable.compas,"Compas","Compas descripcion","1400");
-        AddProduct(R.drawable.transportador,"Transportador","Transportador descripcion","1300");
-        AddProduct(R.drawable.cartuchera,"Cartuchera","Cartuchera descripcion","1400");
-        AddProduct(R.drawable.temperas,"Temperas","Temperas descripcion","1300");
-        AddProduct(R.drawable.pincel,"Pincel","Pincel descripcion","1400");
+        AddProduct(R.drawable.cuaderno,"Cuaderno","Cuaderno pasta dura estilo rayado","3200");
+        AddProduct(R.drawable.carpeta,"Carpeta","Carpeta de carton tamaño carta","4800");
+        AddProduct(R.drawable.compas,"Compas","Compas metalico de precisión ","1400");
+        AddProduct(R.drawable.transportador,"Transportador","Transportador de plastico 180 grados","1300");
+        AddProduct(R.drawable.cartuchera,"Cartuchera","Estuche pequeño para guardar colores o lapices","7400");
+        AddProduct(R.drawable.temperas,"Temperas","Temperas pequeñas colores primarios","5300");
+        AddProduct(R.drawable.pincel,"Pincel","Pincel N° 06 redondo de madera","1400");
 
         productAdapter = new ProductAdapter(this,products);
         listViewProducts.setAdapter(productAdapter);
